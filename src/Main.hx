901b1adfe74c577f9e417d180b72dc3894f6ceb7
Mark Knol <mark.knol@gmail.com>
2016-06-21 22:06:54 +0200

    refactoring/cleanup

--- src/Main.hx
+++ src/Main.hx
@@ -15,231 +15,24 @@ using StringTools;
  * @author Mark Knol
  */
 class Main {
-  static var excludedField = ["haxe_doc","meta","impl"];
-  static function main() {
+  public static function main(){
     var args = Sys.args();
+    if (args.length == 0) {
+      Sys.println("run with 'ApiStats.n' \n" +
+        "\t path to xml directory. default: 'xml/' \n" +
+        "\t path to output file. default: 'xml/index.html' \n" +
+        "\t path to template html-file. default: 'layout.html' \n" +
+        "\t path to log-file. default: 'xml/log.txt' \n");
+    }
     
-    var directory = args[0];
+    inline function getArg(i, fallback) return args[i] == null ? fallback : args[i];
+    
+    var directory = getArg(0, "xml/");
     if (!directory.endsWith("/")) directory += "/";
     
-    var log = "";
-    var totals = new Stats("total");
+    var outputPath = getArg(1, directory + "index.html");
+    var templatePath = getArg(2, "layout.html");
     
-    inline function getTargetName(file:String) return file.replace(".xml", "");
-
-    inline function getDoc(child:Xml):Null<String> {
-      return [for(haxeDoc in child.elementsNamed("haxe_doc")) haxeDoc.firstChild().nodeValue][0];
-    }
-    
-    var documentationByClassPath:StringMap<String> = new StringMap<String>();
-    
-    // fill targets with empty maps
-    var targets = ["toplevel", "sys", "haxe", "js", "cpp", "lua", "java", "macro", "neko", "php", "python", "hl", "flash", "cs"];
-    var stats:StringMap<Stats> = [for (target in targets) target => new Stats(target)];
-    var subStats = new StringMap<Stats>();
-    
-    
-    inline function process(typeInfos:TypeInfos, typeId:TypeId, addStats:Stats->Void = null) {
-      if (typeInfos == null || typeInfos.path == null || typeInfos.isPrivate || !typeInfos.file.endsWith(".hx")) return;
-      
-      var classPath = typeInfos.path;
-      var classPaths = classPath.split(".");
-      classPaths.pop(); // remove class name
-      var packageName = classPaths[0];
-      
-      var stat:Stats = classPaths.length == 0 ? stats.get("toplevel") : stats.get(packageName);
-      if (stat == null) stats.set(packageName, stat = new Stats(packageName));
-      
-      inline function saveStat(stat:Stats) {
-        stat.addClassDoc(typeId, typeInfos, typeInfos.doc);
-        if (addStats != null) addStats(stat);
-      }
-      
-      if (classPaths.length >= 1) {
-        var subPackageName = (classPaths.length == 1) ? classPaths[0] : classPaths[0] + "." + classPaths[1];
-        var subStat:Stats = subStats.get(subPackageName);
-        if (subStat == null) subStats.set(subPackageName, subStat = new Stats(subPackageName, true));
-        saveStat(subStat);
-      }
-      
-      saveStat(stat);
-    }
-    
-    function find(root:Array<TypeTree>) {
-      for (el in root) {
-        switch(el) {
-          case TPackage(name, full, subs): 
-            find(subs);
-            
-          case TClassdecl( c ): 
-            var typeInfos:TypeInfos = c;
-            process(c, TypeId.CLASS, function(stat) {
-              for (field in c.fields) 
-                if (field.isPublic && !field.isOverride) 
-                  stat.addFieldDoc(c, field, field.doc);
-                  
-              for (field in c.statics) 
-                if (field.isPublic && !field.isOverride) 
-                  stat.addFieldDoc(c, field, field.doc);
-            });
-            
-          case TEnumdecl( e ): 
-            process(e, TypeId.ENUM, function(stat) {
-              for (field in e.constructors)
-                stat.addFieldDoc(e, field, field.doc);
-            });
-            
-          case TTypedecl( t ): 
-            process(t, TypeId.TYPEDEF); 
-            
-          case TAbstractdecl( a ):
-            process(a, TypeId.ABSTRACT);
-        }
-      }
-    }
-    
-    var parser = new XmlParser();
-    // fill maps with data
-    for (file in FileSystem.readDirectory(directory)) {
-      if (file.indexOf(".xml") == -1) continue;
-      var xml = Xml.parse(File.getContent(directory + file));
-      var target = getTargetName(file);
-      if (!stats.exists(target)) stats.set(target, new Stats(target));
-      parser.process(xml.firstElement(), getTargetName(file));
-    }
-    
-    find(parser.root);
-    
-    for (stat in stats) {
-      log += stat.toString();
-      totals.add(stat);
-    }
-    
-    log += "-- total -- \n\n";
-    log += totals.toString();
-    
-    //trace(log);
-    
-    File.saveContent(directory + "log.txt", log);
-    
-    var templatePath = args[1];
-    if (templatePath == null) templatePath = "layout.html";
-    var template = Template.fromFile(templatePath);
-    File.saveContent(directory + "index.html", template.execute({
-      stats: [for (stat in stats) stat],
-      subStats: [for (stat in stats) stat.name => [for (s in subStats) if (s.name.startsWith(stat.name)) s]],
-      totals: totals,
-      title: directory.replace("/"," "),
-      Math: Math,
-    }));
+    new StatsBuilder(directory, outputPath, templatePath);
   }
-}
-
-typedef Docs = Array<String>;
-
-@:keep private class StatInfo {
-  public var total:Int = 0;
-  public var documented:Int = 0;
-  
-  public inline function new() {
-  }
-  
-  public function getPercentage() {
-    return toFixed(documented / total * 100.0, 2);
-  }
-  
-  public function add(info:StatInfo) {
-    total += info.total;
-    documented += info.documented;
-  }
-  
-  public function getProgressBar() {
-    var percentage = getPercentage();
-    return '<div class="progress"><div class="progress-bar progress-bar-${Stats.getClassName(percentage)}" style="width: ${percentage}%" aria-valuemin="0" aria-valuemax="100" role="progressbar" aria-valuenow="${Math.floor(percentage)}">${percentage}%</div></div>';
-  }
-  
-  static inline function toFixed(value:Float, precision) {
-    return Math.floor(value * Math.pow(10, precision)) / Math.pow(10, precision);
-  }
-}
-
-@:keep private class Stats {
-  public var name(default, null):String;
-  
-  public var types:StatInfo = new StatInfo();
-  public var fields:StatInfo = new StatInfo();
-  public var all:Array<StatInfo>;
-  
-  public var collectMissingTypes:Bool;
-  public var missingTypes:Array<String> = [];
-  public var missingFields:Array<String> = [];
-  
-  public inline function new(name:String, collectMissingTypes:Bool = false) {
-    this.name = name;
-    this.collectMissingTypes = collectMissingTypes;
-    all = [types, fields];
-  }
-  
-  public function add(stats:Stats) {
-    for (i in 0...stats.all.length) {
-      all[i].add(stats.all[i]);
-    }
-  }
-  
-  private function hasInvalidMetaData(el:{meta:MetaData}) {
-    for (meta in el.meta) if ((meta.name == ":dox" && meta.params[0] == "hide") || meta.name == ":noCompletion") {
-      return true;
-    }
-    return false;
-  }
-  
-  public function addClassDoc(typeId:TypeId, type:TypeInfos, doc:String) {
-    if (hasInvalidMetaData(type)) return;
-    types.total++;
-    if (hasDoc(doc)) {
-      types.documented ++;
-    } else if (collectMissingTypes) {
-      missingTypes.push(type.path);
-    }
-  }
-  
-  public function addFieldDoc(type:TypeInfos, field:{meta:MetaData, name:String}, doc:String) {
-    if (hasInvalidMetaData(field)) return;
-    
-    fields.total++;
-    if (hasDoc(doc)) {
-      fields.documented ++;
-    } else if (collectMissingTypes) {
-      missingFields.push(type.path + "." + field.name);
-    }
-  }
-  
-  inline function hasDoc(doc:String) return (doc != null && doc.length > 1);
-    
-  public function toString() {
-    var v = "";
-    var newline = "\n";
-    v += (name + " types: " + types.total + " - " + types.getPercentage() + "% documented") + newline;
-    v += (name + " members: " + fields.total + " - " + fields.getPercentage() + "% documented") + newline;
-    v += "\n";
-    return v;
-  }
-  
-  public function getClass(percentage:Float) {
-    return getClassName(percentage);
-  }
-  
-  public static function getClassName(percentage:Float) {
-    return if (percentage < 10) "important";
-           else if (percentage > 75) "success";
-           else "warning";
-  }
-  
-}
-
-@:enum abstract TypeId(Int) {
-  public var CLASS = 1;
-	public var ENUM = 2;
-	public var TYPEDEF = 3;
-	public var ABSTRACT = 4;
 }
---
+++ src/StatsBuilder.hx
@@ -0,0 +1,236 @@
+package;
+
+import haxe.ds.StringMap;
+import haxe.rtti.CType.MetaData;
+import haxe.rtti.CType.TypeInfos;
+import haxe.rtti.CType.TypeTree;
+import haxe.rtti.XmlParser;
+import sys.FileSystem;
+import sys.io.File;
+import templo.Template;
+
+using StringTools;
+
+/**
+ * @author Mark Knol
+ */
+class StatsBuilder{
+  static inline var TOPLEVEL = "toplevel";
+  static var baseTargets = [TOPLEVEL];
+  
+  public function new(directory:String, outputPath:String, templatePath:String) {
+    
+    // stats by type path. fill targets with empty maps
+    var stats:StringMap<Stat> = [for (platform in baseTargets) platform => new Stat(platform)];
+    
+    // sub stats by type path. substats are the packages in the packages
+    var subStats = new StringMap<Stat>();
+    
+    // process haxe type. stats can be decorated while collecting
+    inline function process(typeInfos:TypeInfos, typeId:TypeId, decorator:Stat->Void = null) {
+      if (typeInfos == null || typeInfos.path == null || typeInfos.isPrivate || !typeInfos.file.endsWith(".hx")) return;
+      
+      var typePath = typeInfos.path;
+      var typePaths = typePath.split(".");
+      typePaths.pop(); // remove actual type name
+      var packageName = typePaths[0];
+      
+      var stat:Stat = typePaths.length == 0 ? stats.get(TOPLEVEL) : stats.get(packageName);
+      if (stat == null) stats.set(packageName, stat = new Stat(packageName));
+      
+      inline function processType(stat:Stat) {
+        stat.addTypeInfo(typeId, typeInfos, typeInfos.doc);
+        if (decorator != null) decorator(stat);
+      }
+      
+      if (typePaths.length >= 1) {
+        var subPackageName = typePaths.length == 1 ? '$packageName' : '$packageName.${typePaths[1]}';
+        var subStat:Stat = subStats.get(subPackageName);
+        if (subStat == null) subStats.set(subPackageName, subStat = new Stat(subPackageName, true));
+        processType(subStat);
+      }
+      
+      processType(stat);
+    }
+  
+    function hasValidMetaData(el:{meta:MetaData}) {
+      for (meta in el.meta) if ((meta.name == ":dox" && meta.params[0] == "hide") || meta.name == ":noCompletion") {
+        return false;
+      }
+      return true;
+    }
+  
+    // recursive collect+processes all info from TypeTrees
+    function collect(types:Array<TypeTree>) {
+      for (type in types) {
+        switch(type) {
+          case TPackage(name, full, subs): 
+            collect(subs);
+            
+          case TClassdecl(c):
+            var typeInfos:TypeInfos = c;
+            process(c, TypeId.CLASS, function(stat) {
+              for (field in c.fields) 
+                if (field.isPublic && !field.isOverride && hasValidMetaData(field)) 
+                  stat.addFieldInfo(c, field, field.doc);
+                  
+              for (field in c.statics) 
+                if (field.isPublic && !field.isOverride && hasValidMetaData(field)) 
+                  stat.addFieldInfo(c, field, field.doc);
+            });
+            
+          case TEnumdecl(e):
+            process(e, TypeId.ENUM, function(stat) {
+              for (field in e.constructors)
+                if (hasValidMetaData(field))
+                  stat.addFieldInfo(e, field, field.doc);
+            });
+            
+          case TTypedecl(t):
+            process(t, TypeId.TYPEDEF); 
+            
+          case TAbstractdecl(a):
+            process(a, TypeId.ABSTRACT);
+        }
+      }
+    }
+    
+    inline function getPlatformName(file:String) return file.replace(".xml", "");
+    
+    // read xmls in directory. process xml docs using rtti.XmlParser
+    var parser = new XmlParser();
+    for (file in FileSystem.readDirectory(directory)) {
+      if (file.indexOf(".xml") == -1) continue;
+      var xml = Xml.parse(File.getContent(directory + file));
+      var platform = getPlatformName(file);
+      if (!stats.exists(platform)) stats.set(platform, new Stat(platform));
+      parser.process(xml.firstElement(), platform);
+    }
+    
+    // here starts the search for statistics
+    collect(parser.root);
+    
+    // sum up all stats
+    var log = "";
+    var totals = new Stat("total");
+    for (stat in stats) {
+      log += stat.toString();
+      totals.add(stat);
+    }
+    log += "-- total -- \n\n" + totals.toString();
+    trace(log);
+    //File.saveContent(logPath, directory + "log.txt");
+    
+    // load template, execute, save as file
+    var template = Template.fromFile(templatePath);
+    File.saveContent(outputPath, template.execute({
+      stats: [for (stat in stats) stat],
+      subStats: [for (stat in stats) stat.name => [for (s in subStats) if (s.name.startsWith(stat.name)) s]],
+      totals: totals,
+      title: directory.replace("/"," "),
+    }));
+  }
+}
+
+private class StatInfo {
+  public var total:Int = 0;
+  public var documented:Int = 0;
+  
+  public inline function new() { }
+  
+  public function getPercentage() {
+    return toFixed(documented / total * 100.0, 2);
+  }
+  
+  public function add(info:StatInfo) {
+    total += info.total;
+    documented += info.documented;
+  }
+  
+  @:keep public function getProgressBar() {
+    return HtmlTemplateUtils.getProgressBar(getPercentage());
+  }
+  
+  static inline function toFixed(value:Float, precision) {
+    return Math.floor(value * Math.pow(10, precision)) / Math.pow(10, precision);
+  }
+}
+
+@:keep private class Stat {
+  public var name(default, null):String;
+  
+  public var types(default, null):StatInfo = new StatInfo();
+  public var fields(default, null):StatInfo = new StatInfo();
+  
+  public var missingTypes(default, null):Array<String> = [];
+  public var missingFields(default, null):Array<String> = [];
+  
+  private var _all:Array<StatInfo>;
+  private var _collectMissing:Bool;
+  
+  public inline function new(name:String, collectMissing:Bool = false) {
+    this.name = name;
+    
+    _collectMissing = collectMissing;
+    _all = [types, fields];
+  }
+  
+  /** Add stat into this **/
+  public function add(stats:Stat) {
+    for (i in 0 ... stats._all.length) {
+      @:privateAccess _all[i].add(stats._all[i]);
+    }
+  }
+  
+  /** Collect type info **/
+  public function addTypeInfo(typeId:TypeId, type:TypeInfos, doc:String) {
+    types.total++;
+    if (hasDoc(doc)) {
+      types.documented ++;
+    } else if (_collectMissing) {
+      missingTypes.push(type.path);
+    }
+  }
+  
+  /** Collect field info **/
+  public function addFieldInfo(type:TypeInfos, field:{meta:MetaData, name:String}, doc:String) {
+    fields.total++;
+    if (hasDoc(doc)) {
+      fields.documented ++;
+    } else if (_collectMissing) {
+      missingFields.push(type.path + "." + field.name);
+    }
+  }
+  
+  public function toString() {
+    var newline = "\n";
+    var v = '$name types: ${types.documented}/${types.total} = ${types.getPercentage()}% documented $newline';
+    v += '$name members: ${fields.documented}/${fields.total} = ${fields.getPercentage()}% documented $newline$newline';
+    return v;
+  }
+  
+  public function getClassName(percentage:Float) {
+    return HtmlTemplateUtils.getClassName(percentage);
+  }
+  
+  inline function hasDoc(doc:String) return doc != null && doc.length > 1;
+}
+
+class HtmlTemplateUtils {
+  public inline static function getClassName(percentage:Float) {
+    return if (percentage < 10) "important";
+           else if (percentage > 75) "success";
+           else "warning";
+  }
+  
+  static inline public function getProgressBar(percentage:Float) {
+    return '<div class="progress"><div class="progress-bar progress-bar-${getClassName(percentage)}" style="width: ${percentage}%" aria-valuemin="0" aria-valuemax="100" role="progressbar" aria-valuenow="${Math.floor(percentage)}">${percentage}%</div></div>';
+  }
+}
+
+@:enum abstract TypeId(Int) {
+  public var CLASS = 1;
+	public var ENUM = 2;
+	public var TYPEDEF = 3;
+	public var ABSTRACT = 4;
+}
